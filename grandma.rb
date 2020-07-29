b0329cfa7654bea3a8c32a4385017213085b5189
Sierra Wallace <sskwallace@gmail.com>
2020-07-29 01:14:05 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,9 +1,9 @@
 def speak_to_grandma(string)
   if string == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
-  elsif string = string.upcase
+  elsif string == string.upcase
     return "NO, NOT SINCE 1938!"
-  else
+  else string == string.capitalize
     return "HUH?! SPEAK UP,SONNY!"
   end
 end
