15084cc8aaf754620f2422ae985d9fb0bca3a2fe
Sierra Wallace <sskwallace@gmail.com>
2020-07-29 01:05:24 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,4 +1,13 @@
-# Write a speak_to_grandma method.
+def speak_to_grandma(string)
+    if string = "hi".downcase
+    return "HUH?! SPEAK UP, SONNY!"
+  end
+end
+
+  
+
+
+
 
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
